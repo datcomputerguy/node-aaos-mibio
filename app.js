5e8ddc270cfaef7885496bd91ce331fbd6a76d0f
Patrick <datcomputerguy@gmail.com>
2022-03-13 00:07:41 +0000

    whatever

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    //res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
 });
 // Chatroom
